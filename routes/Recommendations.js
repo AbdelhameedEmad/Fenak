214f660a1a5927912e7433094989dba008aa81d0
AbdelhameedEmad <37069108+AbdelhameedEmad@users.noreply.github.com>
2019-04-19 20:16:18 +0200

    working

---
+++ routes/Recommendations.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const py = require('python-shell');
+// const { spawn } = require('child_process');
+
+const recommendations = express.Router();
+const cors = require('cors');
+
+recommendations.use(cors());
+
+recommendations.post('/recommend', (req, res) => {
+  // eslint-disable-next-line no-console
+  console.log(req.body.test);
+  const options = {
+    mode: 'text',
+    pythonOptions: ['-u'],
+    args: [req.body.test],
+  };
+  py.PythonShell.run(process.env.PYTHON_PATH, options, (err, results) => {
+    if (err) throw err;
+    // results is an array consisting of messages collected during execution
+    // eslint-disable-next-line no-console
+    console.log('results: ', results);
+    res.send(results);
+  });
+});
+
+module.exports = recommendations;
